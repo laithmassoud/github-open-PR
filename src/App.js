0944ace1aaefb527790afc94aa6705c36ec9eaf0
laithmassoud <laythmassoud8@gmail.com>
2020-08-22 19:58:42 +0200

    bring the data from api

--- src/App.js
+++ src/App.js
@@ -11,31 +11,25 @@ function App() {
   const fetchData = async () => {
     setLoading(true);
     const response = await axios.get(
-      "https://api.github.com/search/issues?q=+type:pr+user:evannetwork"
+      "https://api.github.com/users/evannetwork/repos"
     );
 
-    setRepos(response.data.items);
+    let repoNames = response.data.map((repo) => repo.name);
+
+    Promise.all(repoNames.map(name =>
+      fetch(`https://api.github.com/repos/evannetwork/${name}/pulls?state=all`).then(resp => resp.json())
+  )).then(res => {
+      console.log(res);
+  })
+
     setLoading(false);
   };
-
+  
   useEffect(() => {
     fetchData();
   }, []);
 
-  return (
-    <div>
-      {loading ? (
-        <div>Loading ....</div>
-      ) : (
-        <ul>
-          {" "}
-          {repos.map((repo) => (
-            <RepoCard key={repo.id} repo={repo} />
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
